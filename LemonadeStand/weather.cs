05ce516754fa5bbcfe074e977e3003c8294ea45b
Jaron <Jaronleach98@gmail.com>
2023-07-26 17:11:37 -0400

    Created weather api to track local forecast.

---
+++ LemonadeStand/weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Http.Headers;
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://weather-api99.p.rapidapi.com/weather?city=District%20of%20Columbia%20"),
+    Headers =
+    {
+        { "X-RapidAPI-Key", "1234910bc5mshb02f16ee596c21bp14aec2jsn374dc053af6e" },
+        { "X-RapidAPI-Host", "weather-api99.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body);
+}
