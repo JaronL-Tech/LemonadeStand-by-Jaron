e1ab8ee041edce9b37f42a4159a7e73114bf7767
Jaron <Jaronleach98@gmail.com>
2023-07-31 12:08:52 -0400

    Created code to have weather affect demand.

--- LemonadeStand/Customer.cs
+++ LemonadeStand/Customer.cs
@@ -19,10 +19,10 @@ public class Customer
         pricePerSugarCube = 0.1;
         pricePerIceCube = 0.1;
         pricePerCup = 0.25;
-        temperature = 37; // Default temperature, you can set this to the actual temperature in your application.
+        temperature = 37; 
     }
 
-    // Method to set the current temperature (you can call this method periodically to update the temperature).
+    
     public void SetTemperature(double tempCelsius)
     {
         temperature = tempCelsius;
@@ -31,28 +31,27 @@ public class Customer
     // Method to calculate the adjusted demand based on temperature and price.
     public double CalculateAdjustedDemand()
     {
-        // Assuming the demand can range from 0.0 (no demand) to 1.0 (full demand).
+        
         double demand = 1.0;
 
-        // Temperature-based adjustment
+       
         if (temperature > 30)
         {
-            // If the temperature is above 30 degrees Celsius, increase demand by 10%.
+            
             demand *= 1.1;
         }
         else if (temperature < 20)
         {
-            // If the temperature is below 20 degrees Celsius, decrease demand by 10%.
+            
             demand *= 0.9;
         }
 
-        // Price-based adjustment
-        // Add your price-based demand adjustments here if needed.
+       
 
         return demand;
     }
 
-    // Method to get the final price based on the adjusted demand and original prices.
+    
     public double CalculateFinalPrice()
     {
         double adjustedDemand = CalculateAdjustedDemand();
